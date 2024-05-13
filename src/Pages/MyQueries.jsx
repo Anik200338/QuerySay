0a0c3252720e33c05c3acaf652f508ff84878eeb
Anik200338 <sarkaranik200338@gmail.com>
2024-05-13 16:58:15 +0600

    Add query banner add

--- src/Pages/MyQueries.jsx
+++ src/Pages/MyQueries.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Add from '../../public/Add.json';
 import MyCardQueries from '../component/Myqueries/MyQueries';
+import Lottie from 'lottie-react';
 
 const MyQueries = () => {
   return (
     <>
-      <div className="">
-        <div className="hero min-h-5 bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <Link to="/AddQueries">
-                <button className="btn btn-primary">Add Queries</button>
-              </Link>
-            </div>
+      <div className="hero  bg-gradient-to-r from-[#00C9FF] to-[#92FE9D] h-96 lg:h-72 rounded-lg p-2">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-2xl font-bold">
+              Empower Your Voice: AddQuery and Let Your Questions Spark Dialogue
+            </h1>
+            <Lottie animationData={Add} className="h-32" />
+            <Link
+              to="/AddQueries"
+              className="relative inline-flex items-center justify-center inline-block p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group"
+            >
+              <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 bg-blue-500 rounded-full blur-md ease"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+                <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-purple-500 rounded-full blur-md"></span>
+                <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-pink-500 rounded-full blur-md"></span>
+              </span>
+              <span className="relative text-white">Add Queries</span>
+            </Link>
           </div>
         </div>
       </div>
